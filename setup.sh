8c3ebb2e5e255cb5cdeb1b6394788a6fae57fa3e
claireij <ruppelclaire@yahoo.de>
2022-08-26 15:56:30 +0200

    Updated Database URL

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #export DATABASE_URL="postgresql://postgres@localhost:5432/postgres"
-export DATABASE_URL="postgres://ryocedmgljtfca:df28ac69c42ace2a5f30588eaf4646e49cf8c132989cfc3145d768d1c151f5ef@ec2-44-193-178-122.compute-1.amazonaws.com:5432/daouqsn0qcv1e6"
+export DATABASE_URL="postgres://izdxwufidiyhnc:c8fb3c199ccb5388b7e82b8b572cff30a250f17b10024aaa17ae6f4ecb71f368@ec2-34-203-182-65.compute-1.amazonaws.com:5432/d5c9dqcif17umm
+"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
